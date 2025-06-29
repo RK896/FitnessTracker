71955f52e1071ca69c329f17413fbe8e0a437932
RK896 <rohit.karnik@outlook.com>
2025-06-29 12:05:59 -0400

    refactor WorkoutController for improved readability and maintainability

--- backend/src/main/java/com/fitness/tracker/controller/WorkoutController.java
+++ backend/src/main/java/com/fitness/tracker/controller/WorkoutController.java
@@ -1,6 +1,5 @@
 package com.fitness.tracker.controller;
 
-import com.fitness.tracker.model.User;
 import com.fitness.tracker.model.Workout;
 import com.fitness.tracker.service.WorkoutService;
 import org.springframework.http.ResponseEntity;
